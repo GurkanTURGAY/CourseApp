b3807522c5fe610351563c119df85a4574aab99b
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-07-24 13:36:58 +0300

    controller and view definitions

---
+++ CourseApp/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseApp.Models
+{
+    public class Candidate
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime AppyAt { get; set; }
+
+        public Candidate()
+        {
+            AppyAt = DateTime.Now;
+        }
+    }
+}
